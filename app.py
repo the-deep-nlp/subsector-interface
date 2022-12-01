c67e77445f57c8a3067d1a3301fa049429aea4d9
Ranjan Shrestha <ranjan.shrestha.np@gmail.com>
2022-12-01 14:16:00 +0545

    docker-compose yaml updated with server configs;

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import sessionmaker
 
 user = "postgres"
 passwd = "postgres"
-host = "localhost"
+host = "db"
 port = 5432
 db = "postgres"
 
@@ -14,38 +14,3 @@ engine = create_engine(url)
 
 Session = sessionmaker(bind=engine)
 session = Session()
-
-
-
-
-
-# from tagger_page import TaggerPage
-
-
-# class NewEntryPage:
-#     def __init__(self):
-#         pass
-
-#     def create_page(self):
-#         st.write("This is a new entry page")
-
-# class SubSector:
-#     def __init__(self):
-#         self.tagger_page = TaggerPage()
-
-#     def handle_tagger_page(self):
-#         tagger_page.create_page()
-    
-#     def handle_newentry_page(self):
-#         new_entry_page.create_page()
-        
-
-# subsector = SubSector()
-# tagger_page = TaggerPage()
-# new_entry_page = NewEntryPage()
-
-# st.sidebar.write("Sub-Sector Menu")
-# st.sidebar.button("Add Tagger", on_click=subsector.handle_tagger_page)
-# st.sidebar.button("New Entry Tag", on_click=subsector.handle_newentry_page)
-# st.sidebar.button("Edit Entry Tag")
-# st.sidebar.button("View Entry Tags")
