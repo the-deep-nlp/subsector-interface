ee09ae2b79ddd3372b86634c71247fe0c386fecc
Ranjan Shrestha <ranjan.shrestha.np@gmail.com>
2022-12-05 14:09:42 +0545

    guidelines updated;

--- guidelines.py
+++ guidelines.py
@@ -7,6 +7,6 @@ def guidelines():
         see tasks assigned in the progress bar. Go through the excerpt and select the relevant Sector, SubSector,
         SubSubSector"""
     )
-    st.sidebar.write("2. Navigate to View Entry Tag page to have an overview of your work.")
-    st.sidebar.write("3. Navigate to Edit Entry Tag page to edit an excerpt you have already tagged.")
-    st.sidebar.write("4. Note that Upload CSV and New Tagger pages are for admins only.")
+    st.sidebar.write("2. Navigate to View Entry Tag page to have an overview of your tagging work.")
+    st.sidebar.write("3. Navigate to Edit Entry Tag page to edit an tagged excerpt you have already tagged.")
+    st.sidebar.write("4. Note that Upload CSV and New Tagger pages are only for admins.")
